981f7ee10728abac3c37f24a53d0a27198162be3
João Pedro Biguelini <biguelinijoao@gmail.com>
2023-04-03 20:13:28 -0300

    Update 5.js

--- 5.js
+++ 5.js
@@ -2,8 +2,8 @@ let palavra = 'exemplo'
 
 let invertido = ''
 
-for (let i = variavel.length - 1; i >= 0; i--) {
-    invertido += variavel[i]
+for (let i = palavra.length - 1; i >= 0; i--) {
+    invertido += palavra[i]
 }
 
 console.log(invertido)
